4395f67b61b7076a1a81b542780dd36f91f6cecd
Coraline Prot <cprot@c1r2p2.42nice.fr>
2025-05-26 14:28:36 +0200

    modif header et split

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cprot <cprot@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 11:26:43 by cprot             #+#    #+#             */
-/*   Updated: 2025/05/22 14:43:39 by cprot            ###   ########.fr       */
+/*   Updated: 2025/05/26 14:17:04 by cprot            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,17 @@
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <stdio.h>
-# include <stdio.h>
 # include <unistd.h>
 
-# define CMD1 1
-# define CMD2 2
-//...
+# define COMMAND 1
+# define ARGUMENT 2
+# define PIPE 3
+# define REDIRECT_IN 4     //
+# define REDIRECT_OUT 5    // >
+# define REDIRECT_APPEND 6 // >>
+# define FILENAME 8
+# define STRING 9 // Contenu entre guillemets
+# define TOKEN_UNKNOWN 0
 
 typedef struct s_list
 {
@@ -32,8 +37,9 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-t_list	*pars_line(char *line);
+t_list				*pars_line(char *line);
 
-static void	create_node(t_list **stack, char *token_str, int token_type);
+static void			create_node(t_list **stack, char *token_str,
+						int token_type);
 
 #endif
--- token.c
+++ token.c
@@ -6,7 +6,7 @@
 /*   By: cprot <cprot@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 14:00:47 by cprot             #+#    #+#             */
-/*   Updated: 2025/05/22 14:46:52 by cprot            ###   ########.fr       */
+/*   Updated: 2025/05/26 13:59:56 by cprot            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,34 @@ t_list	*ft_split_token(char *s)
 	int		j;
 	char	c;
 	char	*str;
+	char	temp[1024];
 	t_list	*list;
 
 	i = 0;
 	j = 0;
-	str = malloc(sizeof(*char));
+	list = NULL;
 	if (!str)
-	{
-		return ;
-	}
+		return NULL;
 	if (s[i] == '"' || s[i] == '\'')
 	{
 		c = s[i];
 		i++;
 		while (s[i] != c)
 		{
-			str[j++] = s[i++];
+			temp[j++] = s[i++];
+			if (s[i] == '\0')
+			{
+				if (c = '"')
+					s = readline("dquote>");
+				else
+					s = readline("quote>");
+			}
+			i = 0;
 		}
-		create_node(&list, str, 0); 
+		temp[j] = '\0';
+		str = malloc(j + 1);
+		ft_strcpy(str, temp);
+		create_node(&list, str, 0);
 	}
+	return (list);
 }
